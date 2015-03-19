1147382286e3cae8addeec91f094976ca4200c95
xiaotian.wu <xiaotian.wu@chinacache.com>
2015-03-19 07:15:47 +0000

    add Makefile

---
+++ src/matrix.cc
@@ -0,0 +1,136 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <iostream>
+#include <string>
+
+#include <mesos/resources.hpp>
+#include <mesos/scheduler.hpp>
+
+using namespace mesos;
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::flush;
+using std::string;
+using std::vector;
+
+using mesos::Resources;
+
+const int32_t CPUS_PER_TASK = 1;
+const int32_t MEM_PER_TASK = 128;
+
+class TestScheduler : public Scheduler
+{
+public:
+  TestScheduler(
+      bool _implicitAcknowledgements,
+      const ExecutorInfo& _executor,
+      const string& _role)
+    : implicitAcknowledgements(_implicitAcknowledgements),
+      executor(_executor),
+      role(_role),
+      tasksLaunched(0),
+      tasksFinished(0),
+      totalTasks(5) {}
+
+  virtual ~TestScheduler() {}
+
+  virtual void registered(SchedulerDriver*,
+                          const FrameworkID&,
+                          const MasterInfo&)
+  {
+    cout << "Registered!" << endl;
+  }
+
+  virtual void reregistered(SchedulerDriver*, const MasterInfo& masterInfo) {}
+
+  virtual void disconnected(SchedulerDriver* driver) {}
+
+  virtual void resourceOffers(SchedulerDriver* driver,
+                              const vector<Offer>& offers)
+  {
+    foreach (const Offer& offer, offers) {
+      //cout << "Received offer " << offer.id() << " with " << offer.resources()
+      //     << endl;
+    }
+  }
+
+  virtual void offerRescinded(SchedulerDriver* driver,
+                              const OfferID& offerId) {}
+
+  virtual void statusUpdate(SchedulerDriver* driver, const TaskStatus& status)
+  {
+  }
+
+  virtual void frameworkMessage(SchedulerDriver* driver,
+                                const ExecutorID& executorId,
+                                const SlaveID& slaveId,
+                                const string& data) {}
+
+  virtual void slaveLost(SchedulerDriver* driver, const SlaveID& sid) {}
+
+  virtual void executorLost(SchedulerDriver* driver,
+                            const ExecutorID& executorID,
+                            const SlaveID& slaveID,
+                            int status) {}
+
+  virtual void error(SchedulerDriver* driver, const string& message)
+  {
+    cout << message << endl;
+  }
+
+private:
+  const bool implicitAcknowledgements;
+  const ExecutorInfo executor;
+  string role;
+  int tasksLaunched;
+  int tasksFinished;
+  int totalTasks;
+};
+
+int main(int argc, char** argv)
+{
+  ExecutorInfo executor;
+  executor.mutable_executor_id()->set_value("default");
+  executor.mutable_command()->set_value("");
+  executor.set_name("Test Executor (C++)");
+  executor.set_source("cpp_test");
+
+  FrameworkInfo framework;
+  framework.set_user(""); // Have Mesos fill in the current user.
+  framework.set_name("Test Framework (C++)");
+  framework.set_role("xiaotian");
+
+  TestScheduler scheduler(true, executor, "xiaotian");
+
+  MesosSchedulerDriver*  driver = new MesosSchedulerDriver(
+        &scheduler,
+        framework,
+        "180.97.185.35:5050");
+
+  int status = driver->run() == DRIVER_STOPPED ? 0 : 1;
+
+  // Ensure that the driver process terminates.
+  driver->stop();
+
+  delete driver;
+  return status;
+}
